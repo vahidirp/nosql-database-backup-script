40baacbfc4a710b834028bcf07e313e16f4a8073
-VAHID-IRP <community.vahid@hotmail.com>
2023-11-23 23:44:02 +0330

    Update mongodb-backup.py

--- mongodb-backup.py
+++ mongodb-backup.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 # Databases dictionary 
 databases_info = [
-    {"name": "db1", "host": "localhost", "port": "27017"},
-    {"name": "db2", "host": "localhost", "port": "27017"},
-    {"name": "db3", "host": "localhost", "port": "27017"}
+    {"name": "db1", "host": "localhost", "port": "27017", "username": "your_username", "password": "your_password"},
+    {"name": "db2", "host": "localhost", "port": "27017", "username": "your_username", "password": "your_password"},
+    {"name": "db3", "host": "localhost", "port": "27017", "username": "your_username", "password": "your_password"}
 ]
 
 backup_path = "/home/mongodb-backups"
@@ -27,7 +27,8 @@ for db_info in databases_info:
 
     zip_file_name = os.path.join(db_dir_path, f"{db_name}_{current_datetime}.zip")
 
-    backup_command = f"mongodump --host {db_host} --port {db_port} --db {db_name} --out {os.path.join(db_dir_path, db_name+'_backup')}"
+    backup_command = f"mongodump --host {db_host} --port {db_port} --db {db_name} --username {db_info['username']} --password {db_info['password']} --out {os.path.join(db_dir_path, db_name+'_backup')}"
+    
     subprocess.run(backup_command, shell=True)
 
     shutil.make_archive(os.path.join(db_dir_path, db_name+"_backup"), 'zip', db_dir_path, db_name+"_backup")
